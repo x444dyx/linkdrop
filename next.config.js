e84c67878178b9c69ebd00a140f3887457ee5b60
Adil <adil.aslam1829@gmail.com>
2026-04-18 00:49:22 +0100

    add supabase storage image domains to next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
+}
 module.exports = nextConfig
